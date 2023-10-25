d24867274d7dba518cb8bdaa032a6da7bf478e5b
Parth Vachhani <vachhani.parth.96@gmail.com>
2023-10-25 15:26:23 -0400

    Completed Part 2. Task 5.6 wsa the most difficult, partially due to the wording being confusing for a while

--- app.js
+++ app.js
@@ -2,9 +2,20 @@
 // Menu data structure
 const menuLinks = [
     {text: 'about', href: '/about'},
-    {text: 'catalog', href: '/catalog'},
-    {text: 'orders', href: '/orders'},
-    {text: 'account', href: '/account'},
+    {text: 'catalog', href: '#', subLinks: [
+        {text: 'all', href: '/catalog/all'},
+        {text: 'top selling', href: '/catalog/top'},
+        {text: 'search', href: '/catalog/search'},
+    ]},
+    {text: 'orders', href: '#' , subLinks: [
+        {text: 'new', href: '/orders/new'},
+        {text: 'pending', href: '/orders/pending'},
+        {text: 'history', href: '/orders/history'},
+    ]},
+    {text: 'account', href: '#', subLinks: [
+        {text: 'profile', href: '/account/profile'},
+        {text: 'sign out', href: '/account/signout'},
+    ]},
 ];
 
 const mainEl = document.querySelector('main');
@@ -25,3 +36,93 @@ for(let menu of menuLinks) {
 }
 
 //Part 2
+//4.0 - 4.5
+const subMenuEl = document.getElementById('sub-menu'); //4.0
+subMenuEl.style.height='100%'; //4.1
+subMenuEl.style.backgroundColor = ('var(--sub-menu-bg)'); //4.2
+subMenuEl.setAttribute('class','flex-around'); //4.3
+subMenuEl.style.position = 'absolute'; //4.4
+subMenuEl.style.top = '0'; //4.5
+
+//5.1
+const topMenuLinks = document.querySelectorAll("#top-menu a");
+console.log(topMenuLinks)
+
+let showingSubMenu = false;
+
+//5.2-5.9
+topMenuEl.addEventListener('click', function(e) { //5.2
+    e.preventDefault();
+    // console.dir(menuLinks);
+    // console.dir(e.target)
+    if (e.target.tagName !== 'A') {
+        return
+    }
+
+    if(e.target.classList.contains('active')) { //5.3
+        e.target.classList.remove('active');
+        showingSubMenu = false;
+        subMenuEl.style.top = 0;
+        return
+    }
+
+    for(let link of topMenuLinks) { //5.4
+        link.classList.remove('active')
+    }
+
+    if(e.target.tagName === 'A') { //5.5
+        e.target.classList.add('active');
+    }
+
+    const subLinkExists = menuLinks.find(function(link) { //5.6
+        if(link.text === e.target.innerHTML) {
+            // console.log(link.subLinks)
+            // console.log(e.target.innerHTML)
+            return e.target.innerHTML;
+        }
+    })
+
+    if('subLinks' in subLinkExists) {
+        showingSubMenu = true;
+    } else {
+        showingSubMenu = false;
+    }
+
+    
+    if(showingSubMenu) { //5.7
+        buildSubMenu(subLinkExists.subLinks)
+        subMenuEl.style.top = '100%';
+        mainEl.innerHTML = `<h1>${e.target.innerHTML}</h1>` 
+    } else {
+        subMenuEl.style.top = 0;
+        mainEl.innerHTML = '<h1>about</h1>'
+    }
+
+    function buildSubMenu(subLinks){ //5.8
+        subMenuEl.innerHTML = '';
+        for(let subLink of subLinks) {
+            const linkEl = document.createElement('a');
+            linkEl.setAttribute('href', subLink.href);
+            linkEl.innerHTML = subLink.text;
+            subMenuEl.appendChild(linkEl);
+        }
+    }
+})
+
+//6.0 - 6.3
+subMenuEl.addEventListener('click', function(e) { //6.0
+    e.preventDefault();
+    if(e.target.tagName !=='A') {
+        return
+    }
+    // console.log(e.target.textContent)
+
+    showingSubMenu = false; //6.1
+    subMenuEl.style.top = 0;
+
+    for(let link of topMenuLinks) { //6.2
+        link.classList.remove('active')
+    }
+
+    mainEl.innerHTML = `<h1>${e.target.textContent}</h1>` //6.3
+})
